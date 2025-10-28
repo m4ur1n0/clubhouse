c6bc3aae16a063ca77d356a7251bdedb7f84b779
NeoTrovela <ntrovelavillamiel@gmail.com>
2025-10-28 09:55:18 -0500

    Added command to backfill clubs and events with a nil user_id

--- db/migrate/20251028002739_make_clubs_user_id_not_null.rb
+++ db/migrate/20251028002739_make_clubs_user_id_not_null.rb
@@ -1,5 +1,9 @@
 class MakeClubsUserIdNotNull < ActiveRecord::Migration[8.0]
   def change
+    # Backfill clubs with nil user_id
+    u = User.first || User.create!(name: "System Owner", email: "owner@example.com", google_id: SecureRandom.uuid)
+    Club.where(user_id: nil).update_all(user_id: u.id)
+    
     change_column_null :clubs, :user_id, false
   end
 end
--- db/migrate/20251028002852_make_events_user_id_not_null.rb
+++ db/migrate/20251028002852_make_events_user_id_not_null.rb
@@ -1,5 +1,9 @@
 class MakeEventsUserIdNotNull < ActiveRecord::Migration[8.0]
   def change
+    # Backfill events with nil user_id
+    u = User.first || User.create!(name: "System Owner", email: "owner@example.com", google_id: SecureRandom.uuid)
+    Event.where(user_id: nil).update_all(user_id: u.id)
+    
     change_column_null :events, :user_id, false
   end
 end
