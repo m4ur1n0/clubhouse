f0fa8192a6e7c9ba75af8cb7c64bd667dc54ba03
NeoTrovela <ntrovelavillamiel@gmail.com>
2025-11-13 09:51:41 -0600

    Added message editing features
    Edit or delete messages in clubs, able to show timestamps of when the message was edited

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_05_214823) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_13_153645) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_05_214823) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "edited_at"
     t.index ["club_id", "created_at"], name: "index_chat_messages_on_club_id_and_created_at"
     t.index ["club_id"], name: "index_chat_messages_on_club_id"
     t.index ["user_id"], name: "index_chat_messages_on_user_id"
