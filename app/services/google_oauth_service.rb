9bee44bc12be3ee70293e16f3e557437035d916f
theo <thjmaurino@gmail.com>
2025-11-14 15:03:19 -0600

    added google refresh service for refreshing the token

---
+++ app/services/google_oauth_service.rb
@@ -0,0 +1,23 @@
+class GoogleOuathService
+    TOKEN_URL = 'https://oauth2.googleapis.com/token'
+
+    def self.refresh!(user)
+        return unless user.google_refresh_token.present?
+
+        response = HTTParty.post(TOKEN_URL, body: {
+            client_id: ENV.fetch("GOOGLE_CLIENT_ID"),
+            client_secret: ENV.fetch("GOOGLE_CLIENT_SECRET"),
+            refresh_token: user.google_refresh_token,
+            grant_type: 'refresh_token'
+        })
+
+        if response.success?
+            user.update(
+                google_access_token: response['access_token'],
+                google_token_expires_at: Time.current + response['expires_in'].to_i
+            )
+        else
+            Rails.logger.error("Failed to refresh Google access token for user #{user.id}: #{response.body}")
+        end
+    end
+end
